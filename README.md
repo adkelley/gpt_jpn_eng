77e0b8108ddb76766b6c823732fa455679b393d7
adkelley <adkelley@mac.com>
2023-09-06 16:15:34 -0700

    mvp completed

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
-# ChatGPT English <> Japanese Conversion
+# ChatGPT English <> Japanese Conversion Inline
 
 ## User Stories
-1. As a User, I can select one or more lines of English or Japanese text and convert it, in place, 
-to Japanese or English, respectively.
+1. As a User, I can select one or more lines of English or Japanese text in a 
+document and convert it, inline, to Japanese or English, respectively.
 2. As a User, I should be able to specify the context and the degree of formality in the desired 
 translation
-
-
 
 ## UI Notes
 ### UI Steps
 1. Select the text
 2. Copy the selected text
-3. Enter a keyboard shortcut, via TextExpander
+3. Enter a snippet shortcut, via TextExpander
 4. Text is replaced with translated version
 
 ## Programming Notes
 1. Use TextExpander to enter a keyboard shortcut
+2. Uses [chatgpt_rs](https://docs.rs/chatgpt_rs/1.2.1/chatgpt/index.html) crate
+
+## Todo
+1. Add streaming reponses feature to begin printing the translation immediately
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
-fn main() {
-    // 1. Concatentate the arguments to form sentences
-    // let args: Vec<String> = env::args().collect();
-    // let sentences = &args[1..];
-    // let concatenated = sentences.join(" ");
+use anyhow::{Error, Result};
+use chatgpt::config::{ModelConfiguration, ModelConfigurationBuilder};
+use chatgpt::prelude::*;
 
-    // 2. Ask ChatGPT to determine whether the sentences in English or Japanese
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // 1. Concatentate the arguments to form sentences
+    let args: Vec<String> = std::env::args().collect();
+    let words = &args[1..];
+    let sentences = words.join(" ");
+
+    // 2. Ask ChatGPT to translate whether the sentences in English or Japanese
+    let key = dotenvy::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set");
+    let config: ModelConfiguration = ModelConfigurationBuilder::default()
+        .temperature(0.0)
+        .max_tokens(100 as u32) // this could be a problem!
+        .build()
+        .unwrap_or_default();
+    let client = ChatGPT::new_with_config(key, config)?;
+
+    let conversation = r#"
+            You're an expert at translating Japanese or English languages.  Please translate the 
+            following sentences into Japanese or English, depending on whether the original sentences 
+            are in English or Japanese.  Please respond with the translation only and do not specify
+            whether the translated sentence is Japanese or English.
+            "#;
+    let conversation_string = conversation.to_string();
+    let mut conversation: Conversation = client.new_conversation_directed(&conversation_string);
+
+    let translation = conversation.send_message(&sentences).await?;
+    println!("{}", translation.message_choices[0].message.content);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -19,63 +45,35 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_02_detect_jpn() {
+    async fn test_02_translate() {
         let result = async {
+            use chatgpt::config::{ModelConfiguration, ModelConfigurationBuilder};
             use chatgpt::prelude::*;
 
-            let key = dotenvy::var("OPENAI_KEY").expect("OPEN_AI key must be set");
-            let client = ChatGPT::new(key)?;
+            let key = dotenvy::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set");
+            let config: ModelConfiguration = ModelConfigurationBuilder::default()
+                .max_tokens(32 as u32)
+                .build()
+                .unwrap_or_default();
+            let client = ChatGPT::new_with_config(key, config)?;
 
             let conversation = r#"
-                You're an expert at detecting whether sentences are in the Japanese or English language.
-                What is the language of the following sentence?  
-                Respond by stating the just the language (English or Japanese)
+            You're an expert at translating Japanese or English languages.  Please translate the 
+            following sentences into Japanese or English, depending on whether the original sentences 
+            are in English or Japanese.  Please respond with the translation only.
             "#;
             let conversation_string = conversation.to_string();
             let mut conversation: Conversation =
                 client.new_conversation_directed(&conversation_string);
 
-            let sentences = "私はアレクスーケリーです。".to_string();
+            let sentences = "I am Alex Kelley.".to_string();
             conversation.send_message(&sentences).await
         };
 
         match result.await {
             Ok(cr) => {
-                let language = &cr.message_choices[0].message.content;
-                assert_eq!(language, "Japanese");
-            }
-            Err(e) => {
-                println!("Error: {:#?}", e);
-                assert!(false);
-            }
-        }
-    }
-
-    #[tokio::test]
-    async fn test_03_detect_eng() {
-        let result = async {
-            use chatgpt::prelude::*;
-
-            let key = dotenvy::var("OPENAI_KEY").expect("OPEN_AI key must be set");
-            let client = ChatGPT::new(key)?;
-
-            let conversation = r#"
-                You're an expert at detecting whether sentences are in the Japanese or English language.
-                What is the language of the following sentence?  
-                Respond by stating the just the language (English or Japanese)
-            "#;
-            let conversation_string = conversation.to_string();
-            let mut conversation: Conversation =
-                client.new_conversation_directed(&conversation_string);
-
-            let sentences = "I am Alex Kelley".to_string();
-            conversation.send_message(&sentences).await
-        };
-
-        match result.await {
-            Ok(cr) => {
-                let language = &cr.message_choices[0].message.content;
-                assert_eq!(language, "English");
+                let translation = &cr.message_choices[0].message.content;
+                assert_eq!(translation, "私はあれくすケリーです。");
             }
             Err(e) => {
                 println!("Error: {:#?}", e);
